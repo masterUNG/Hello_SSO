ad3e45670e1d014951ff42e8ee01feb0eac1329e
masterUNG <phrombutr@gmail.com>
2019-02-06 09:53:50 +0700

    UPdate

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,12 @@ import 'package:flutter/material.dart';
 void main() {
   var app = MaterialApp(
     home: Scaffold(
-      appBar: AppBar(leading: Text('Leading'),
+      appBar: AppBar(
+        leading: Icon(Icons.arrow_back),
         title: Text('This is AppBar'),
       ),
     ),
   );
-
-
-  
 
   runApp(app);
 }

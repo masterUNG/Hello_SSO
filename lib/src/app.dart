36e16b7947f49e39f19fa7cf1cfe3fd61f3a71a0
masterUNG <phrombutr@gmail.com>
2019-02-06 12:03:41 +0700

    update 12.03

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget{
+class App extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return AppState();
+  }
+}
+
+class AppState extends State<App>{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build

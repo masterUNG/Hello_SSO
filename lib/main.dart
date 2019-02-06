df131160199e22751b827ff0b848025c821479cd
masterUNG <phrombutr@gmail.com>
2019-02-06 10:31:14 +0700

    update 10.30

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,9 @@ void main() {
         leading: Icon(Icons.arrow_back),
         title: Text('This is AppBar'),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+      ),
     ),
   );
 

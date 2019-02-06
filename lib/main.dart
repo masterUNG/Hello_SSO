724b61f25c49ec248fa57d829ff7ad13158acc5e
masterUNG <phrombutr@gmail.com>
2019-02-06 11:52:24 +0700

    update 11.52

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,6 @@
 import 'package:flutter/material.dart';
+import 'src/app.dart';
 
 void main() {
-  var app = MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        actions: <Widget>[
-          Icon(Icons.android),
-          Icon(Icons.apps),
-          Icon(Icons.arrow_drop_down_circle)
-        ],
-        leading: Icon(Icons.arrow_back),
-        title: Text('This is AppBar'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add_circle_outline),
-        onPressed: () {
-          print('You Click Floating');
-        },
-      ),
-    ),
-  );
-
-  runApp(app);
+  runApp(App());
 }

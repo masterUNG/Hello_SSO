baa84c9828d2ee52f3111ac90a23a6219d37b8d2
masterUNG <phrombutr@gmail.com>
2019-02-06 10:23:10 +0700

    update 10.23

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,12 @@ import 'package:flutter/material.dart';
 void main() {
   var app = MaterialApp(
     home: Scaffold(
-      appBar: AppBar(actions: <Widget>[Icon(Icons.android)],
+      appBar: AppBar(
+        actions: <Widget>[
+          Icon(Icons.android),
+          Icon(Icons.apps),
+          Icon(Icons.arrow_drop_down_circle)
+        ],
         leading: Icon(Icons.arrow_back),
         title: Text('This is AppBar'),
       ),

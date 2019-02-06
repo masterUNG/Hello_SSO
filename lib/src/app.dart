5985645a81a39dee085f91164f7e826061a0d553
masterUNG <phrombutr@gmail.com>
2019-02-06 13:20:10 +0700

    Counter

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class App extends StatefulWidget{
+class App extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
@@ -8,30 +8,35 @@ class App extends StatefulWidget{
   }
 }
 
-class AppState extends State<App>{
+class AppState extends State<App> {
+
+  int counter = 0;
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    
     return MaterialApp(
-    home: Scaffold(
-      body: Text('This is Body'),
-      appBar: AppBar(
-        actions: <Widget>[
-          Icon(Icons.android),
-          Icon(Icons.apps),
-          Icon(Icons.arrow_drop_down_circle)
-        ],
-        leading: Icon(Icons.arrow_back),
-        title: Text('This is AppBar'),
+      home: Scaffold(
+        body: Text('counter ==> $counter'),
+        appBar: AppBar(
+          actions: <Widget>[
+            Icon(Icons.android),
+            Icon(Icons.apps),
+            Icon(Icons.arrow_drop_down_circle)
+          ],
+          leading: Icon(Icons.arrow_back),
+          title: Text('This is AppBar'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add_circle_outline),
+          onPressed: () {
+            setState(() {
+              counter += 1;
+              print('counter = $counter');
+            });
+          },
+        ),
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add_circle_outline),
-        onPressed: () {
-          print('You Click Floating');
-        },
-      ),
-    ),
-  );
+    );
   }
-
 }
